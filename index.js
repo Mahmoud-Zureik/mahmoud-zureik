6ea182b2f599677ab6b3dba3719c458622bca980
Mahmoud Zureik <zureikmahmoud105@gmail.com>
2026-08-29 06:03:00 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+function welcome() {
+  Swal.fire({
+    title: "مرحباً بك 👋",
+    text: "يسعدني وجودك في موقعي 🌟",
+    imageUrl: "imgs/image.png",
+    imageWidth: 300,
+    imageHeight: 200,
+    imageAlt: "Welcome",
+    confirmButtonText: "دخول إلى الموقع",
+    footer: `
+      <a dir="rtl" href="https://youtube.com/channel/UCvbXfC0LwUgtxDksR5hGgdA?si=Nlhn2KW5-YfjdGKM"
+         target="_blank">
+          اشترك في قناتي على YouTube
+      </a>
+    `
+  });
+}
+
 const navbar = document.querySelector(".navbar");
 const bars = document.querySelector(".fa-bars");
 const xmark = document.querySelector(".fa-xmark");
